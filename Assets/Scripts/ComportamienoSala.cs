9b9b1f68ebdf5a63355a501fa77badd79c1932e8
Alejandro-Moles <amoleshurtado@gmail.com>
2023-03-16 12:44:41 +0100

    Initial commit

---
+++ Assets/Scripts/ComportamienoSala.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamienoSala : MonoBehaviour
+{
+    public GameObject[] muros; // 0 - top, 1 - down, 2 - left, 3 - rigth
+    public GameObject[] puertas; //mismo orden
+
+    public bool[] testStatus;
+
+    private void Start()
+    {
+        UpdateSala(testStatus);
+    }
+
+    void UpdateSala(bool[] status)
+    {
+        for(int i = 0; i< status.Length; i++)
+        {
+            puertas[i].SetActive(status[i]);
+            muros[i].SetActive(!status[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/GeneradorMazmorra.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GeneradorMazmorra : MonoBehaviour
+{
+  
+    public class Celda
+    {
+        public bool visitada = false;
+        public bool[] status = new bool[4]; 
+    }
+
+    public Vector2Int size;
+    public int posicionInicio;
+    List<Celda> tablero;
+
+
+
+    //Metodo Generador de laberinto
+    void GeneradorLaberinto()
+    {
+        tablero = new List<Celda>();
+
+        for(int i = 0;i < size.x; i++)
+        {
+            for(int j = 0;j<size.y; j++)
+            {
+                tablero.Add(new Celda());
+            }
+        }
+
+        int celdaActual = posicionInicio;
+
+        Stack<int> camino = new Stack<int>();
+
+        int num = 0;
+
+        while(num < 100)
+        {
+            num++;
+            tablero[celdaActual].visitada = true;
+
+            //comprobar celdas vecinas
+            List<int> vecinos = CompruebaVecinos(celdaActual);
+
+            #region No hay Casillas Alrededor
+            //comprobamos si no hay casillas alrededor
+            if (vecinos.Count <= 0)
+            {
+                //si no hay mas camino que recorrer
+                if(camino.Count <= 0)
+                {
+                    break; //Salimos
+                }
+                //retrocedemos en el camino
+                else
+                {
+                    celdaActual = camino.Pop();
+                }
+            }
+            #endregion
+
+            #region Si hay Casillas Alrededor
+            //si hay casillas alrededor
+            if (vecinos.Count > 0)
+            {
+                camino.Push(celdaActual); //añadimos la celda al camino
+
+                int nuevaCelda = vecinos[Random.Range(0,vecinos.Count)];//Asignamos la nueva aleatoriamente
+
+                #region Puertas de abajo y derecha
+                //derecha o abajo
+                if (nuevaCelda > celdaActual)
+                {
+                    if(nuevaCelda -1 == celdaActual) //derecha
+                    {
+                        tablero[celdaActual].status[3] = true; //colocamos la puerta derecha
+                        celdaActual = nuevaCelda;
+                        tablero[celdaActual].status[2] = true; //colocamos la puerta izquierda en la nueva celda
+                    }
+
+                    else //vecino de abajo
+                    {
+                        tablero[celdaActual].status[1] = true; //colocamos la puerta abajo
+                        celdaActual = nuevaCelda;
+                        tablero[celdaActual].status[0] = true; //colocamos la puerta arriba en la nueva celda
+                    }
+                }
+                #endregion
+
+                #region Puertas de arriba e izquierda
+                else //vecino de arriba o izquierda
+                {
+                    if (nuevaCelda + 1 == celdaActual) //izquierda
+                    {
+                        tablero[celdaActual].status[2] = true; //colocamos la puerta izquierda
+                        celdaActual = nuevaCelda;
+                        tablero[celdaActual].status[3] = true; //colocamos la puerta derecha en la nueva celda
+                    }
+
+                    else //vecino de arriba
+                    {
+                        tablero[celdaActual].status[9] = true; //colocamos la puerta arriba
+                        celdaActual = nuevaCelda;
+                        tablero[celdaActual].status[1] = true; //colocamos la puerta abajo en la nueva celda
+                    }
+                }
+                #endregion
+            }
+            #endregion
+        }
+    }
+
+    List<int> CompruebaVecinos(int celda)
+    {
+        List<int> vecinos = new List<int>();
+
+        //comprobamos vecino de arriba
+        if(celda - size.x >= 0 && !tablero[celda - size.x].visitada)
+        {
+            vecinos.Add(celda - size.x);
+        }
+
+        //comprobamos el de abajo
+        if(celda + size.x < tablero.Count && !tablero[celda + size.x].visitada)
+        {
+            vecinos.Add(celda + size.x);
+        }
+
+        //comprobamos el de la izquierda
+        if (celda % size.x != 0 && !tablero[celda - 1].visitada)
+        {
+            vecinos.Add(celda % size.x);
+        }
+
+        //comprobamos el de la derecha
+        if ((celda+1) % size.x != 0 && !tablero[celda + 1].visitada)
+        {
+            vecinos.Add((celda + 1) % size.x);
+        }
+
+        return vecinos;
+    }
+}
