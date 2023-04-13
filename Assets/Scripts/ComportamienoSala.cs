7f38b2449122b989c34cf0368e974853e47c47b4
Alejandro-Moles <amoleshurtado@gmail.com>
2023-04-13 10:50:14 +0200

    v4

--- Assets/Scripts/ComportamienoSala.cs
+++ Assets/Scripts/ComportamienoSala.cs
@@ -1,27 +1,30 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ComportamienoSala : MonoBehaviour
 {
-    public GameObject[] muros; // 0 - top, 1 - down, 2 - left, 3 - rigth
-    public GameObject[] puertas; //mismo orden
+    public GameObject[] muros, puertas; // 0 -top, 1 - down, 2- left, 3-right
+    bool conectada;
+    //public bool[] testStatus;
 
-    
-    public bool[] testStatus;
 
-    private void Start()
-    {
-        UpdateSala(testStatus);
-    }
-    
     public void UpdateSala(bool[] status)
     {
-        for(int i = 0; i< status.Length; i++)
+        conectada = false;
+        for (int i = 0; i < status.Length; i++)
         {
             puertas[i].SetActive(status[i]);
             muros[i].SetActive(!status[i]);
+            if (status[i])
+            {
+                conectada = true;
+            }
+        }
+        if (!conectada)
+        {
+            Destroy(gameObject);
         }
     }
+
 }
--- Assets/Scripts/GeneradorMazmorra.cs
+++ Assets/Scripts/GeneradorMazmorra.cs
@@ -5,28 +5,29 @@ using UnityEngine.UIElements;
 
 public class GeneradorMazmorra : MonoBehaviour
 {
-  
     public class Celda
     {
         public bool visitada = false;
-        public bool[] status = new bool[4]; 
+        public bool[] status = new bool[4]; // 0 -top, 1 - down, 2- left, 3-right
     }
 
     public Vector2Int size;
     public int posicionInicio = 0;
     public GameObject sala;
-    public Vector2 dimension;
+    public Vector2 dimension; //separacion entre salas
+    public int longitud;
+
+
     List<Celda> tablero;
-
-
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
         GeneradorLaberinto();
     }
 
+    // Update is called once per frame
 
-    void GenerarMazmorra()
+    void GeneraMazmorra()
     {
         for (int i = 0; i < size.x; i++)
         {
@@ -38,14 +39,13 @@ public class GeneradorMazmorra : MonoBehaviour
         }
     }
 
-    //Metodo Generador de laberinto
     void GeneradorLaberinto()
     {
         tablero = new List<Celda>();
 
-        for(int i = 0;i < size.x; i++)
+        for (int i = 0; i < size.x; i++)
         {
-            for(int j = 0;j<size.y; j++)
+            for (int j = 0; j < size.y; j++)
             {
                 tablero.Add(new Celda());
             }
@@ -53,113 +53,97 @@ public class GeneradorMazmorra : MonoBehaviour
 
         int celdaActual = posicionInicio;
 
-        Stack<int> camino = new Stack<int>();
+        Stack<int> camino = new Stack<int>(); // pila
 
         int num = 0;
 
-        while(num < 100)
+        while (num < longitud)
         {
             num++;
             tablero[celdaActual].visitada = true;
-
             //comprobar celdas vecinas
             List<int> vecinos = CompruebaVecinos(celdaActual);
 
-            #region No hay Casillas Alrededor
-            //comprobamos si no hay casillas alrededor
-            if (vecinos.Count <= 0)
+            //comprobamos si no hay vecinos
+
+            if (vecinos.Count == 0)
             {
-                //si no hay mas camino que recorrer
-                if(camino.Count <= 0)
+                //si no hay mas caminos que probar
+                if (camino.Count == 0)
                 {
-                    break; //Salimos
+                    break; //salimos
                 }
-                //retrocedemos en el camino
-                else
+                else //retrocedmos
                 {
-                    celdaActual = camino.Pop();
+                    celdaActual = camino.Pop(); // popo saca cosas de la pila
                 }
             }
-            #endregion
-
-            #region Si hay Casillas Alrededor
-            //si hay casillas alrededor
-            if (vecinos.Count > 0)
+            else // si hay vecinos
             {
-                camino.Push(celdaActual); //añadimos la celda al camino
+                camino.Push(celdaActual);
+                int nuevaCelda = vecinos[UnityEngine.Random.Range(0, vecinos.Count)]; //asiganamos la nueva celda de forma aleatoria
 
-                int nuevaCelda = vecinos[Random.Range(0,vecinos.Count)];//Asignamos la nueva aleatoriamente
 
-                #region Puertas de abajo y derecha
-                //derecha o abajo
-                if (nuevaCelda > celdaActual)
+                if (nuevaCelda > celdaActual) // vecino derecha o abajo
                 {
-                    if(nuevaCelda -1 == celdaActual) //derecha
+                    if (nuevaCelda - 1 == celdaActual) //derecha 
                     {
-                        tablero[celdaActual].status[3] = true; //colocamos la puerta derecha
+                        tablero[celdaActual].status[3] = true; //abrir puerta derecha
                         celdaActual = nuevaCelda;
-                        tablero[celdaActual].status[2] = true; //colocamos la puerta izquierda en la nueva celda
+                        tablero[celdaActual].status[2] = true; //abrir puerta izquierda de la nueva celda
                     }
-
-                    else //vecino de abajo
+                    else // abajo
                     {
-                        tablero[celdaActual].status[1] = true; //colocamos la puerta abajo
+                        tablero[celdaActual].status[0] = true; //abrir puerta arriba
                         celdaActual = nuevaCelda;
-                        tablero[celdaActual].status[0] = true; //colocamos la puerta arriba en la nueva celda
+                        tablero[celdaActual].status[1] = true; //abrir puerta abajo de la nueva celda
                     }
                 }
-                #endregion
-
-                #region Puertas de arriba e izquierda
-                else //vecino de arriba o izquierda
+                else // vecino izquierda o arriba
                 {
                     if (nuevaCelda + 1 == celdaActual) //izquierda
                     {
-                        tablero[celdaActual].status[2] = true; //colocamos la puerta izquierda
+                        tablero[celdaActual].status[2] = true; //abrir puerta izquierda
                         celdaActual = nuevaCelda;
-                        tablero[celdaActual].status[3] = true; //colocamos la puerta derecha en la nueva celda
+                        tablero[celdaActual].status[3] = true; //abrir puerta derecha de la nueva celda
                     }
-
-                    else //vecino de arriba
+                    else // arriba
                     {
-                        tablero[celdaActual].status[0] = true; //colocamos la puerta arriba
+                        tablero[celdaActual].status[1] = true; //abrir puerta abajo
                         celdaActual = nuevaCelda;
-                        tablero[celdaActual].status[1] = true; //colocamos la puerta abajo en la nueva celda
+                        tablero[celdaActual].status[0] = true; //abrir puerta arriba de la nueva celda
                     }
                 }
-                #endregion
+
+
+
             }
-            #endregion
         }
-        GenerarMazmorra();
+        GeneraMazmorra();
     }
 
-    List<int> CompruebaVecinos(int celda)
+    List<int> CompruebaVecinos(int celda) //celda que le mandes te manda los vecinos
     {
         List<int> vecinos = new List<int>();
 
-        //comprobamos vecino de arriba
-        if(celda - size.x >= 0 && !tablero[celda - size.x].visitada)
+        if (celda - size.x >= 0 && !tablero[celda - size.x].visitada) // -size.x comprueba arriba
         {
             vecinos.Add(celda - size.x);
         }
 
-        //comprobamos el de abajo
-        if(celda + size.x < tablero.Count && !tablero[celda + size.x].visitada)
+        if (celda + size.x < tablero.Count && !tablero[celda + size.x].visitada) // -size.x comprueba abajo
         {
             vecinos.Add(celda + size.x);
         }
 
-        //comprobamos el de la izquierda
-        if (celda % size.x != 0 && !tablero[celda - 1].visitada)
+        if (celda % size.x != 0 && !tablero[celda - 1].visitada) // -size.x comprueba izquierda
         {
-            vecinos.Add(celda % size.x);
+            vecinos.Add(celda - 1);
         }
 
-        //comprobamos el de la derecha
-        if ((celda+1) % size.x != 0 && !tablero[celda + 1].visitada)
+        if ((celda + 1) % size.x != 0 && !tablero[celda + 1].visitada) // -size.x comprueba derecha
         {
-            vecinos.Add((celda + 1) % size.x);
+            vecinos.Add(celda + 1);
         }
 
         return vecinos;
